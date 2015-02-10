3fb23106a7774e330f0aa07743716436616d3878
jxv <jxv@hush.com>
2015-02-10 09:28:45 -0800

    Add Page and Generator data types. Add div tags and head tag.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+import Control.Applicative
 import Text.Markdown
 import Text.Blaze.Html (Html)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
@@ -13,6 +14,31 @@ main = do
     fileBaseName:_ <- getArgs
     run (fileBaseName ++ ".md") (fileBaseName ++ ".html")
 
+data Page = Page
+    { pageTitle :: Maybe TL.Text
+    , pageContent :: TL.Text
+    } deriving (Show, Eq)
+
+data Generator a = Generator
+    { genWrapper :: TL.Text -> TL.Text
+    , genStyleSheets :: [TL.Text]
+    , genScripts :: [TL.Text]
+    , genHeader :: TL.Text
+    , genFooter :: TL.Text
+    , genValue :: a
+    }
+
+instance Functor Generator where
+    fmap f gen@Generator{..} = gen { genValue = f genValue }
+
+instance Applicative Generator where
+    pure a = Generator id [] [] TL.empty TL.empty a
+    genFunc <*> gen = gen { genValue = (genValue genFunc) (genValue gen) }
+
+instance Monad Generator where
+    Generator{..} >>= f = f genValue
+    return = pure
+
 run :: FilePath -> FilePath -> IO ()
 run inputFile outputFile = do
     fileContent <- TL.readFile inputFile
@@ -21,5 +47,5 @@ run inputFile outputFile = do
 translate :: TL.Text -> TL.Text
 translate content = begin <> (renderHtml . markdown def $ content) <> end
  where
-    begin = "<!doctype html><html><body>"
-    end = "</body></html>"    
+    begin = "<!doctype html><html><head><title></title></head><body><div id=\"wrapper\"><div id=\"header\"></div><div id=\"content\">"
+    end = "</div><div id=\"footer\"></div></div></body></html>"    
