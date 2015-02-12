c677bc3708cac9239cd9aca1a432455a48a4770a
jxv <jxv@hush.com>
2015-02-11 20:46:55 -0800

    Add config and init argv parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
-import Prelude hiding (head)
-import Control.Applicative
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TL
+import qualified Data.List as L
 import Options.Applicative
 import System.FilePath.Find
 import System.FilePath.Glob
@@ -7,29 +8,64 @@ import System.FilePath.Manip
 import Text.Markdown
 import Text.Blaze.Html (Html)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
-import qualified Data.Text.Lazy as TL
-import qualified Data.Text.Lazy.IO as TL
 import Data.Default
 import Data.Monoid
 import System.Environment
 import Data.Maybe
 
+data Config = Config {
+    cfgHeader :: Bool,
+    cfgFooter :: Bool,
+    cfgArchive :: Bool,
+    cfgStyleSheets :: [FilePath],
+    cfgScripts :: [FilePath],
+    cfgTargetDirectory :: FilePath
+} deriving Show
+
+data Page = Page {
+    pageTitle :: Maybe TL.Text,
+    pageContent :: TL.Text
+} deriving Show
+
+data Template = Template {
+    tplStyleSheets :: [TL.Text],
+    tplScripts :: [TL.Text],
+    tplHeader :: Maybe TL.Text,
+    tplFooter :: Maybe TL.Text
+} deriving Show
+
 main :: IO ()
 main = do
-    fileBaseName:_ <- getArgs
-    run (fileBaseName ++ ".md") (fileBaseName ++ ".html")
+    cfg <- execParser (info cfgP idm)
+    tpl <- mkTemplate cfg
+    page <- mkPage "index.md"
+    TL.writeFile "index.html" (construct tpl page)
+    
+cfgP :: Parser Config
+cfgP = Config
+    <$> switch (long "header" <> help "Include header.md")
+    <*> switch (long "footer" <> help "Include footer.md")
+    <*> switch (long "archive" <> help "Generate archive.html page")
+    <*> pure []
+    <*> pure []
+    <*> pure "."
 
-data Page = Page
-    { pageTitle :: Maybe TL.Text
-    , pageContent :: TL.Text
-    } deriving (Show, Eq)
+mkTemplate :: Config -> IO Template
+mkTemplate Config{..} = do
+    let tplStyleSheets = []
+    let tplScripts = []
+    tplHeader <- go cfgHeader (render <$> TL.readFile "header.md")
+    tplFooter <- go cfgFooter (render <$> TL.readFile "footer.md")
+    return Template{..}
+ where
+    go False _ = return Nothing
+    go True x = fmap Just x
 
-data Template = Template
-    { tplStyleSheets :: [TL.Text]
-    , tplScripts :: [TL.Text]
-    , tplHeader :: Maybe TL.Text
-    , tplFooter :: Maybe TL.Text
-    } deriving (Show, Eq)
+mkPage :: FilePath -> IO Page
+mkPage filePath = do
+    let pageTitle = Nothing
+    pageContent <- render <$> TL.readFile filePath
+    return Page{..}
 
 run :: FilePath -> FilePath -> IO ()
 run inputFilePath outputFilePath = do
@@ -44,15 +80,14 @@ translate content = construct (Template [] [] Nothing Nothing) (Page Nothing (re
 
 construct :: Template -> Page -> TL.Text
 construct Template{..} Page{..} = (html . TL.concat)
-    [ (head . TL.concat) (try pageTitle : map styleSheet tplStyleSheets ++ map script tplScripts)
-    , (body . wrapper . TL.concat) [divHeader (try tplHeader), divContent pageContent, divFooter (try tplFooter)]
-    ]
+    [ (head' . TL.concat) (try pageTitle : map styleSheet tplStyleSheets ++ map script tplScripts)
+    , (body . wrapper . TL.concat) [divHeader (try tplHeader), divContent pageContent, divFooter (try tplFooter)] ]
  where
     try = fromMaybe TL.empty
 
-html, head, title, styleSheet, script, body, wrapper, divHeader, divContent, divFooter :: TL.Text -> TL.Text
+html, head', title, styleSheet, script, body, wrapper, divHeader, divContent, divFooter :: TL.Text -> TL.Text
 html x = "<!doctype html><html>" <> x <> "</html>"
-head x = "<head>" <> x <> "</head>"
+head' x = "<head>" <> x <> "</head>"
 title x = "<title>" <> x <> "</title>"
 styleSheet x = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" <> x <> "\">"
 script x = "<script src=\"" <> x <> "\"></script>"
