5df6d964324d96147ba29c900b8de66a35c7349e
jxv <jxv@hush.com>
2015-03-21 22:23:28 -0700

    Add Lucid; Display date in archive

--- src/Stagen/Build.hs
+++ src/Stagen/Build.hs
@@ -1,5 +1,6 @@
 module Stagen.Build where
 
+import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TL
 import qualified Data.List as L
@@ -17,6 +18,7 @@ import Text.Blaze.Html (Html)
 import Data.Default
 import Data.Monoid
 import Data.Maybe
+import Lucid
 
 import Stagen.Opts
 import Stagen.Date
@@ -37,14 +39,17 @@ runBuild opts@Opts{..} = do
     void $ runJobs optsJobs jobs
 
 build :: Template -> Page -> TL.Text
-build Template{..} Page{..} = (html . TL.concat)
-    [ (head' . TL.concat) (title pageTitle : map styleSheet tplStyleSheets ++ map script tplScripts)
-    , (body . wrapper . TL.concat)
-        [ divHeader (try tplHeader)
-        , divContent pageContent
-        , divFooter (try tplFooter) ] ]
+build Template{..} Page{..} = renderText $ doctypehtml_ $ do
+    head_ $ do
+        void $ title_ (toHtmlRaw pageTitle)
+        mapM_ (\href -> link_ [rel_ "stylesheet", type_ "text/css", href_ href]) (map TL.toStrict tplStyleSheets)
+        mapM_ (\src -> termWith "script" [src_ src] "") (map TL.toStrict tplScripts)
+    body_ . div_ [id_ "wrapper"] $ do
+        div_ [id_ "header"] (try tplHeader)
+        div_ [id_ "content"] (toHtmlRaw pageContent)
+        div_ [id_ "footer"] (try tplFooter)
  where
-    try = fromMaybe TL.empty
+    try = toHtmlRaw . fromMaybe TL.empty
 
 archiveJob :: Verbose -> Template -> [(FilePath, Page)] -> Maybe FilePath -> IO ()
 archiveJob verbose tpl htmlPathAndPages mMdPath = fromMaybe (return ()) $ do
@@ -72,10 +77,26 @@ addArchiveEntries page htmlPathAndPages =
     getEntries = catMaybes . map (\(f,t) -> fmap (f,t,) (parseMay datePrefix (baseName f)))
 
     toLinks :: [(FilePath, TL.Text, Date)] -> TL.Text
-    toLinks = ul . TL.concat . map toLink
+    toLinks = renderText . ul_ . toHtmlRaw . TL.concat . map toLink
 
     toLink :: (FilePath, TL.Text, Date) -> TL.Text
-    toLink (path, title, date) = li (anchor (TL.pack path) title)
+    toLink (path, title, date) =
+        renderText
+            (li_ (a_ [href_ (T.pack path)]
+            (toHtmlRaw $ displayDate date <> " &#8212; <i>" <> title <> "</i>")))
+
+displayDate :: Date -> TL.Text
+displayDate Date{..} = TL.concat 
+    [ TL.pack (show dateYear)
+    , "-"
+    , TL.pack (showTwoDigits dateMonth)
+    , "-"
+    , TL.pack (showTwoDigits dateDay)
+    ]
+ where
+    showTwoDigits n
+        | n < 10 = '0' : show n
+        | otherwise = show n
 
 parseMay :: P.Parser a -> String -> Maybe a
 parseMay p src = case P.parse p "" src of
@@ -99,18 +120,3 @@ mkTemplate Opts{..} = do
  where
     go Nothing = return Nothing
     go (Just path) = Just <$> render <$> TL.readFile path
-
-html, head', title, styleSheet, script, body, wrapper, ul, li, divHeader, divContent, divFooter :: TL.Text -> TL.Text
-html x = "<!doctype html><html>" <> x <> "</html>"
-head' x = "<head>" <> x <> "</head>"
-title x = "<title>" <> x <> "</title>"
-styleSheet x = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" <> x <> "\">"
-script x = "<script src=\"" <> x <> "\"></script>"
-body x = "<body>" <> x <> "</body>"
-wrapper x = "<div id=\"wrapper\">" <> x <> "</div>"
-ul x = "<ul>" <> x <> "</ul>"
-li x = "<li>" <> x
-anchor s x = "<a href=\"" <> s <> "\">" <> x <> "</a>"
-divHeader x = "<div id=\"header\">" <> x <> "</div>"
-divContent x = "<div id=\"content\">" <> x <> "</div>"
-divFooter x = "<div id=\"footer\">" <> x <> "</div>"
